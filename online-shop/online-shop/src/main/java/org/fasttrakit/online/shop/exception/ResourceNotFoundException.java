d1918da8eab5fe965f36e73b81ef1cbb043184aa
Iulian Axinte <axinte.iulian@gmail.com>
2020-02-26 21:27:34 +0300

    added get product by id ( studied lamdas, optional and custom exception)

---
+++ online-shop/online-shop/src/main/java/org/fasttrakit/online/shop/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package org.fasttrakit.online.shop.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
--- online-shop/online-shop/src/main/java/org/fasttrakit/online/shop/service/ProductService.java
+++ online-shop/online-shop/src/main/java/org/fasttrakit/online/shop/service/ProductService.java
@@ -1,12 +1,15 @@
 package org.fasttrakit.online.shop.service;
 
 import org.fasttrakit.online.shop.domain.Product;
+import org.fasttrakit.online.shop.exception.ResourceNotFoundException;
 import org.fasttrakit.online.shop.persistance.ProductRepository;
 import org.fasttrakit.online.shop.transfer.SaveProductRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -33,6 +36,25 @@ private static final Logger LOGGER = LoggerFactory.getLogger(ProductService.clas
         product.setImageUrl(request.getImageUrl());
 
    return productRepository.save(product);
+}
+
+    public Product getProduct(long id) {
+    LOGGER.info("Retrieving product {}", id);
+
+    //optional usage explained
+//        Optional<Product> productOptional = productRepository.findById(id);
+//
+//        if(productOptional.isPresent()){
+//            return productOptional.get();
+//        }else{
+//            throw new ResourceNotFoundException("Product" + id + "not found.");
+//        }
+
+        // lamda expressions
+        productRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Product" + id + "not found."));
+
+
+
 }
 }
 
